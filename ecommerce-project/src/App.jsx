e310c592a4ab987ddd8b1828c922c67220e7734c
Shubham Bhandari <shubh.gehu@gmail.com>
2026-06-15 20:23:20 +0530

    routing added

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,13 @@
-
+import {Routes,Route} from 'react-router';
 import './App.css'
 import { HomePage } from './pages/HomePage'
 function App() {
 
   return (
-    <HomePage></HomePage>)
-}
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/checkout" element={<div> checkout page </div>}/>
+    </Routes>
+  )}
 
 export default App
