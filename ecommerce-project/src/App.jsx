0ce401eae9d03be1d6dc2b4a2f537d082b917ce9
Shubham Bhandari <shubh.gehu@gmail.com>
2026-06-15 21:27:16 +0530

    checkout done

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import {Routes,Route} from 'react-router';
 import './App.css'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage';
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
-      <Route path="/checkout" element={<div> checkout page </div>}/>
+      <Route path="/checkout" element={<CheckoutPage/>}/>
     </Routes>
   )}
 
