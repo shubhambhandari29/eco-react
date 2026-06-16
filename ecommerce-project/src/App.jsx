96ce5f7a5c71f802c6b2d0b6f5dd7550ee97c334
Shubham Bhandari <shubh.gehu@gmail.com>
2026-06-16 13:05:32 +0530

    orders added

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,14 @@ import {Routes,Route} from 'react-router';
 import './App.css'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/checkout" element={<CheckoutPage/>}/>
+      <Route path="/orders" element={<OrdersPage/>}/>
     </Routes>
   )}
 
