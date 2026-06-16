4f288cf479eeb39ad11681c5e8a965fb95a000f4
Shubham Bhandari <shubh.gehu@gmail.com>
2026-06-16 18:57:58 +0530

    cartitems

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { Header } from '../components/Header'
 import { useEffect , useState} from 'react'
 import './HomePage.css'
 import axios from 'axios'
-
+import { formatMoney } from '../utils/money'
 export function HomePage({cart}) {
   const[products, setProducts]=useState([]);
  
@@ -40,7 +40,7 @@ export function HomePage({cart}) {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2)}
+                  ${formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
